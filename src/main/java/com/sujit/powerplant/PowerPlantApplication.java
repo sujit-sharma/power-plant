54824a5a284aa7717e57e709b0c4d2721e600bcc
sujit-sharma <sujit.dhakal1166@gmail.com>
2023-03-04 19:33:20 +0545

    power plant spring boot application created

---
+++ src/main/java/com/sujit/powerplant/PowerPlantApplication.java
@@ -0,0 +1,13 @@
+package com.sujit.powerplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerPlantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerPlantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sujit/powerplant/PowerPlantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujit.powerplant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerPlantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
